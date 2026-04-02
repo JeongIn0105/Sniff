46382d9ac7e94ab9aceb9682f4bbf2226ae89077
이정인 <vbnmj123@naver.com>
2026-04-02 21:26:43 +0900

    최종 프로젝트 파일 생성

---
+++ Sniff/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sniff
+//
+//  Created by 이정인 on 4/2/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
